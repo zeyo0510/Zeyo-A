f4c74d3d478c94a9b1603eebdec3c2ce756fd505
Gary Lee <57802150+garylee0510@users.noreply.github.com>
2022-12-15 15:11:48 +0800

    v2022-12-15 15:11

--- bin/main.js
+++ bin/main.js
@@ -1,3 +1,4 @@
 import { default as $Extremum } from '../lib/$Mathematical/$Extremum.js';
 /************************************************/
+console.log($Extremum.$Maximum());
 console.log($Extremum.$Maximum(50, [60, 120, 90], 80));
