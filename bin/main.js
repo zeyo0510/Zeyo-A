3c5eca233bb0269da69a4e37d3617420180dad85
Zeyo Li <zeyo0510@gmail.com>
2022-12-20 01:05:46 +0800

    v2022-12-20 01:05

--- bin/main.js
+++ bin/main.js
@@ -1,4 +1,43 @@
-import { default as $Extremum } from '../lib/$Mathematical/$Extremum.js';
-/************************************************/
-console.log($Extremum.$Maximum());
-console.log($Extremum.$Maximum(50, [60, 120, 90], 80));
+(() => {
+  globalThis.$Zeyo = globalThis.$Zeyo || {};
+  globalThis.$Zeyo.$App = class $ extends HTMLElement
+  {
+    constructor()
+    {
+      super();
+      /************************************************/
+      const app = [];
+      app.push('AudioPlayerOnline');
+      app.push('VideoPlayerOnline');
+      /************************************************/
+      const example = [];
+      example.push('BoxModel');
+      example.push('Button');
+      /************************************************/
+      const shadow = super.attachShadow({ mode: 'open' });
+      shadow.innerHTML = `
+      <ul>
+      <li>/app</li>
+      <ul>
+        ${app
+        .map((callbackfn) => {
+          return `<li><a href="./app/${callbackfn}">${callbackfn}</a></li>`;
+        })
+        .join('')
+        }
+      </ul>
+      <li>/example</li>
+      <ul>
+        ${example
+        .map((callbackfn) => {
+          return `<li><a href="./example/${callbackfn}">${callbackfn}</a></li>`;
+        })
+        .join('')
+        }
+      </ul>
+    </ul>
+      `;
+    }
+  }
+  globalThis.customElements.define('zeyo-app', globalThis.$Zeyo.$App);
+})();
